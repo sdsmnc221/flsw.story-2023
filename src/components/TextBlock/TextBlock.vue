956ad922c9a59cab1bdb5c80a75fcabb7fb8e4e8
sdsmnc221 <antr.2201@gmail.com>
2024-01-24 04:58:37 +0100

    feat(app): big up: grid animate chapter 1

--- src/components/TextBlock/TextBlock.vue
+++ src/components/TextBlock/TextBlock.vue
@@ -1,26 +1,47 @@
 <template>
   <section
     class="text-block"
+    :class="{ 'text-block--spacing': fx !== 'fx3' }"
     :style="`background-color: var(--${background}); color: var(--${textColor});`"
   >
-    <p
-      v-if="subtitle"
-      class="text-block__subtitle josefin-sans-300 fx2"
-      data-splitting
+    <div class="grid" :class="id" v-if="id === 'section--1'">
+      <div class="grid-wrap">
+        <div class="grid__item" v-for="index in 48" :key="`grid-item-${index}`">
+          <div
+            class="grid__item-inner"
+            style="background-image: url(img/1.jpg)"
+          ></div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="text text--bottom text--left text--absolute"
+      v-if="fx !== 'fx3'"
     >
-      {{ subtitle }}
-    </p>
-    <h2
-      v-if="title"
-      class="text-block__title yeseva-one-regular fx2"
-      data-splitting
-    >
-      {{ title }}
-    </h2>
+      <p
+        v-if="subtitle"
+        class="text-block__subtitle josefin-sans-300 fx2"
+        data-splitting
+      >
+        {{ subtitle }}
+      </p>
+      <h2
+        v-if="title"
+        class="text-block__title yeseva-one-regular fx2"
+        data-splitting
+      >
+        {{ title }}
+      </h2>
+      <div class="text-block__content" :class="`${fx}`" data-splitting>
+        <slot />
+      </div>
+    </div>
     <div
       class="text-block__content"
-      :class="`${fx} ${fx === 'fx3' ? 'josefin-sans-600' : ''}`"
+      :class="`${fx} josefin-sans-600`"
       data-splitting
+      v-else
     >
       <slot />
     </div>
@@ -34,6 +55,7 @@ interface Props {
   background?: string;
   textColor?: string;
   fx?: string;
+  id: string;
 }
 
 withDefaults(defineProps<Props>(), {
@@ -45,14 +67,30 @@ withDefaults(defineProps<Props>(), {
 
 <style lang="scss">
 .text-block {
+  --perspective: 1500px;
+  --grid-item-ratio: 1.5;
+  --grid-width: 100%;
+  --grid-height: auto;
+  --grid-gap: 2vw;
+  --grid-columns: 4;
+  --grid-inner-scale: 1;
+
   width: 100vw;
   min-height: 100vh;
-  padding: 16%;
   display: flex;
-  text-align: left;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  text-align: left;
+
+  position: relative;
+  // margin-bottom: 20vh;
+  // padding-bottom: 32vh;
+
+  &--spacing {
+    padding-bottom: 64vh;
+  }
 
   &:has(.fx3) {
     background: linear-gradient(
@@ -63,20 +101,62 @@ withDefaults(defineProps<Props>(), {
   }
 
   &__title {
-    font-size: 2rem;
-    padding: 0 24%;
-    text-align: center;
+    width: 100%;
+    font-size: 2.4rem;
     line-height: 2rem;
   }
 
   &__subtitle {
-    font-size: 1rem;
+    align-self: flex-start;
+    font-size: 1.2rem;
+    line-height: 1.2rem;
     text-transform: lowercase;
   }
 
   &__content {
-    padding: 0 24%;
+    width: 100%;
     margin-top: 32px;
+    font-size: 1.2rem;
+    line-height: 1.6rem;
+    position: relative;
+
+    &:not(.fx3) {
+      &::after {
+        content: "";
+        position: absolute;
+        top: 10%;
+        left: 10%;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+        mix-blend-mode: color-burn;
+        transition: all 1.2s ease-in-out;
+      }
+      &::before {
+        content: "";
+        position: absolute;
+        top: -5%;
+        left: -10%;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        z-index: -1;
+        mix-blend-mode: color-burn;
+        transition: all 1.2s ease-in-out;
+      }
+
+      &.--background {
+        &::after {
+          background-color: var(--clear-day-white);
+          opacity: 1;
+        }
+
+        &::before {
+          background-color: var(--clear-day-white);
+          opacity: 1;
+        }
+      }
+    }
 
     &.fx3 {
       text-transform: uppercase;
@@ -84,6 +164,70 @@ withDefaults(defineProps<Props>(), {
       padding: 0 16%;
       text-align: center;
       line-height: 3.2rem;
+      padding: 0 24%;
+    }
+  }
+
+  .text {
+    width: 100vw;
+
+    padding: 16%;
+
+    display: flex;
+    text-align: left;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    &--absolute {
+      position: absolute;
+    }
+
+    &--bottom {
+      bottom: 8vh;
+    }
+
+    &--left {
+      padding-right: 48%;
+    }
+  }
+
+  .grid {
+    display: grid;
+    place-items: center;
+    padding: 2rem;
+    width: 100%;
+    perspective: var(--perspective);
+
+    transform: translateY(-16vh);
+
+    &-wrap {
+      height: var(--grid-height);
+      width: var(--grid-width);
+      display: grid;
+      grid-template-columns: repeat(var(--grid-columns), 1fr);
+      gap: var(--grid-gap);
+      transform-style: preserve-3d;
+    }
+
+    &__item {
+      aspect-ratio: var(--grid-item-ratio);
+      width: 100%;
+      height: auto;
+      overflow: hidden;
+      position: relative;
+      border-radius: 8px;
+      display: grid;
+      place-items: center;
+      mix-blend-mode: color-burn;
+
+      &-inner {
+        position: relative;
+        width: calc(1 / var(--grid-inner-scale) * 100%);
+        height: calc(1 / var(--grid-inner-scale) * 100%);
+        background-size: cover;
+        background-position: 50% 50%;
+      }
     }
   }
 
--- src/helpers/scrollFx.ts
+++ src/helpers/scrollFx.ts
@@ -1,3 +1,4 @@
+import { getGrid } from "./getGrid";
 import Lenis from "@studio-freight/lenis";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -9,8 +10,9 @@ let lenis: any;
 // Initialize Lenis smooth scrolling
 const initSmoothScrolling = () => {
   lenis = new Lenis({
-    lerp: 0.2,
+    lerp: 0.1,
     smooth: true,
+    smoothWheel: true, // Enables smooth scrolling for mouse wheel events
   } as any);
 
   lenis.on("scroll", () => ScrollTrigger.update());
@@ -23,16 +25,100 @@ const initSmoothScrolling = () => {
   requestAnimationFrame(scrollFn);
 };
 
+const applyCollageAnimation = (grid: any, animationType: string) => {
+  // Child elements of grid
+  const gridWrap = grid.querySelector(".grid-wrap");
+  const gridItems = grid.querySelectorAll(".grid__item");
+  const gridItemsInner = [...gridItems].map((item) =>
+    item.querySelector(".grid__item-inner")
+  );
+
+  // Define GSAP timeline with ScrollTrigger
+  const timeline = gsap.timeline({
+    defaults: { ease: "none" },
+    scrollTrigger: {
+      trigger: gridWrap,
+      start: "top bottom+=5%",
+      end: "bottom top-=5%",
+      scrub: true,
+    },
+  });
+
+  // Apply different animations based on type
+  switch (animationType) {
+    case "cllg-fx1":
+      // Set some CSS related style values
+      grid.style.setProperty("--grid-width", "120%");
+      grid.style.setProperty("--grid-columns", "8");
+      grid.style.setProperty("--grid-gap", "0");
+
+      const gridObj = getGrid(gridItems);
+
+      timeline
+        .set(gridWrap, {
+          rotationX: 50,
+        })
+        .to(gridWrap, {
+          rotationX: 30,
+        })
+        .fromTo(
+          gridItems,
+          {
+            filter: "brightness(72%) contrast(0.7)",
+          },
+          {
+            filter: "brightness(100%) contrast(2)",
+          },
+          0
+        )
+        .to(
+          gridObj.rows("even"),
+          {
+            xPercent: -100,
+            ease: "power1",
+          },
+          0
+        )
+        .to(
+          gridObj.rows("odd"),
+          {
+            xPercent: 100,
+            ease: "power1",
+          },
+          0
+        )
+        .addLabel("rowsEnd", ">-=0.15")
+        .to(
+          gridItems,
+          {
+            ease: "power1",
+            yPercent: () => gsap.utils.random(-100, 200),
+          },
+          "rowsEnd"
+        );
+      break;
+
+    default:
+      console.error("Unknown animation type.");
+      break;
+  }
+};
+
 // GSAP Scroll Triggers
-const scroll = (fx: { id: string; nodes: any[] }) => {
+const scroll = (fx: { id: string; nodes: any[] }, scrollGrid = false) => {
   switch (fx.id) {
     case "fx1":
+      function getProgress(t) {
+        const currProgress = t;
+        console.log(t);
+      }
+
       fx.nodes.forEach((title) => {
         gsap.fromTo(
           title,
           {
             transformOrigin: "0% 50%",
-            rotate: 3,
+            rotate: 6,
           },
           {
             ease: "none",
@@ -42,6 +128,21 @@ const scroll = (fx: { id: string; nodes: any[] }) => {
               start: "top bottom",
               end: "top top",
               scrub: true,
+              onUpdate: (self) => {
+                const { progress } = self;
+
+                if (progress > 0.2) {
+                  if (!title.classList.contains("--background")) {
+                    title.classList.add("--background");
+                  }
+                }
+
+                if (progress > 0.8) {
+                  if (title.classList.contains("--background")) {
+                    title.classList.remove("--background");
+                  }
+                }
+              },
             },
           }
         );
@@ -78,8 +179,9 @@ const scroll = (fx: { id: string; nodes: any[] }) => {
         gsap.fromTo(
           chars,
           {
-            "will-change": "opacity, transform",
+            "will-change": "opacity, transform, backgorund-color",
             opacity: 0,
+            backgroundColor: "transparent",
             rotateX: () => gsap.utils.random(-160, 160),
             z: () => gsap.utils.random(-240, 240),
             y: () => gsap.utils.random(-120, 120),
@@ -88,6 +190,7 @@ const scroll = (fx: { id: string; nodes: any[] }) => {
           {
             ease: "none",
             opacity: 1,
+            backgroundColor: "var(--clear-day-white)",
             rotateX: 0,
             scale: 1,
             z: 0,
@@ -153,4 +256,10 @@ const scroll = (fx: { id: string; nodes: any[] }) => {
   }
 };
 
-export { initSmoothScrolling, scroll };
+const scrollGrid = (grid: { id: string; node: any }) => {
+  if (grid.node) {
+    applyCollageAnimation(grid.node, grid.id);
+  }
+};
+
+export { initSmoothScrolling, scroll, scrollGrid };
